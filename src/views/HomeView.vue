1438dd4622456dd1203ff77b827c05577bca7459
Hisham Salameh <salamehh@msu.edu>
2022-11-18 00:53:16 -0500

    cas final

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,384 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="home flex justify-center bg-black">
+    <header
+      class="header mx-10 h-[550px] flex flex-col gap-10 items-center justify-center w-[1200px]"
+    >
+      <img
+        src="@/assets/headshot.png"
+        class="w-60 rounded-full border-2 bg-white"
+        alt=""
+      />
+      <div class="header-text flex flex-col gap-5">
+        <h1 class="section-header text-white text-5xl font-extrabold">
+          Land your first Job or Internship
+        </h1>
+        <p class="text-white/90 text-lg font-medium">
+          Hello, my name is Hisham Salamen, and I will teach you how to land
+          your first internship and job with me. With clear vision and strategy
+          I was able to make myself stand out as a self-taught developer, and
+          here are some tips on how I did it.
+        </p>
+        <div class="socials flex justify-center gap-6">
+          <a
+            href="https://www.github.com/salamhis2019"
+            target="blank"
+            class="duration-100 hover:scale-110"
+          >
+            <i class="fab fa-github-square text-white text-5xl"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/hishamsalameh"
+            target="blank"
+            class="duration-100 hover:scale-110"
+          >
+            <i class="fab fa-linkedin text-white text-5xl"></i>
+          </a>
+        </div>
+      </div>
+    </header>
+  </div>
+  <div
+    class="content-parent-container flex justify-center w-full min-h-full bg-white py-5"
+  >
+    <div
+      class="container w-[700px] min-h-full flex border shadow-2xl rounded-xl p-6 content-start"
+    >
+      <section class="content-container bg-white min-h-full w-full">
+        <div
+          class="content-header h-[200px] items-center flex justify-evenly gap-5"
+        >
+          <p class="text-[120px] text-black font-extrabold">1.</p>
+          <p class="section-header text-6xl font-extrabold text-black">
+            Your Resume... <span class="italic">Obviously!</span>
+          </p>
+        </div>
+        <div class="explanation-container flex justify-center">
+          <div class="w-[700px] flex flex-col gap-5">
+            <p>
+              Did you know that recruiters look at resumes for an average of
+              <span class="font-extrabold italic">8 seconds</span>
+            </p>
+            <p>
+              Your resume is going to be the first thing that recruiters are
+              going see. Think of your resume as
+              <span class="italic font-semibold"
+                >the bait on the hook to bring the recruiter in.</span
+              >
+            </p>
+            <h3 class="subheader text-4xl font-extrabold">Some Tips</h3>
+            <p>
+              If you care and love the job, then put in the time to refine your
+              resume for that job!
+            </p>
+            <p>
+              Im sure you've heard of the saying you get what you put in, well
+              it's no different in the job world. Recruiters are well aware when
+              they see resumes that are resused and general, and the best way to
+              stand out is to make sure your resume matches that specific role
+              and company you are applying for.
+            </p>
+            <h3 class="subheader text-4xl font-extrabold">
+              So, what can you do?
+            </h3>
+            <p>
+              Look for keywords, really take a look at what the job posting is
+              saying. I would first starts off by looking at the
+              <span class="italic font-bold">Hard Skills</span>, which are
+              coding languages, programs, creation etc,.
+            </p>
+            <p>
+              Highlight those clearly in your skills section so recruiters can
+              scan over and see you match the criteria.
+            </p>
+            <h4 class="text-2xl font-extrabold">
+              Don't use a online template!
+            </h4>
+            <p>
+              Often times companies use resume scanners to find matches for
+              keywords found in the job description.
+            </p>
+            <p>
+              Most of the time, escpecially if you are applying for a big
+              company, odds are you are initially not encouraging a human that
+              you are worthy but rather
+              <span class="italic font-bold">a computer!</span>
+            </p>
+            <p>
+              Computers have a one way mindset for looking at documents, the
+              simply scan over and see what matches. The issue with templates is
+              that <span class="span font-bold italic">they do too much!</span>
+            </p>
+            <p>
+              Templates often time use all the coolest features that word has to
+              offer to make the template look awesome by using tables, graphs,
+              images etc,. Computes have a hard time interpreting what exactly
+              is being shown if the content is dug within a table, and then
+              <span class="font-extrabold">BOOM!</span> In 7 seconds you have
+              been determined as not worthy. :(
+            </p>
+            <h4 class="text-2xl font-extrabold">
+              So how do I format my resume then?
+            </h4>
+            <p>Keep these things in mind:</p>
+            <ol>
+              <li>1. Keep your document layout as flat as possible</li>
+              <li>
+                2. Avoid fancy templates and create your own resume.
+                <span class="font-extrabold italic">Simple is Better!</span>
+              </li>
+              <li>
+                3. Add in color, avoid images and fancy fonts, keep the font
+                sizes small
+              </li>
+              <li>
+                And to be quite literal, resumes are boring, and its better that
+                way!
+              </li>
+            </ol>
+            <h3 class="subheader text-4xl font-extrabold">
+              What about the content?
+            </h3>
+            <p>
+              Creating resume content is crucial to your success in the job
+              world, and here are my tips.
+            </p>
+            <h4 class="text-2xl font-extrabold">Be Concise</h4>
+            <p>
+              Lets be honest, no one likes to read... and recruiters
+              <span class="font-extrabold">DEFINITELY</span> don't like to read.
+              So, be concise. Highlight the most exciting aspects of your role,
+              and crucial things you did.
+            </p>
+            <p>
+              Avoid using big fancy words. Include only relevant experiences
+              that a recruiter would like to see. Remove any redundency, don't
+              intentionally make your content longer. And lastly, watch for
+              vagueness. Be straight to the point and describe what you did.
+            </p>
+            <h4 class="text-2xl font-extrabold">
+              Use <span class="font-extrabold">Action Verbs!</span>
+            </h4>
+            <p>
+              Here is an awesome resource that highlights some awesome
+              <a
+                href="https://www.themuse.com/advice/185-powerful-verbs-that-will-make-your-resume-awesome"
+                target="blank"
+                class="underline duration-150 hover:text-green-500"
+                >Action Verbs</a
+              >
+              you can use when you are creating your resume!
+            </p>
+            <h3 class="subheader text-4xl font-extrabold">Stand out!</h3>
+            <p>
+              Show who you are, what you like to do. Express your energy and
+              your passions! You are not a robot!
+            </p>
+            <p>
+              On my resume I like to add a hobbies section. I think this holds a
+              lot of value as I feel recruiters like to see what else I do.
+              Also, this can call for really easy conversation when the time
+              comes for an interview (hopefully).
+            </p>
+          </div>
+        </div>
+        <div
+          class="content-header h-[200px] items-center flex justify-evenly gap-5"
+        >
+          <p class="text-[120px] text-black font-extrabold">2.</p>
+          <p class="section-header text-5xl font-extrabold text-black">
+            Network, Network, and... Network.
+          </p>
+        </div>
+        <div class="explanation-container flex justify-center">
+          <div class="w-[700px] flex flex-col gap-5">
+            <p>
+              You've probably heard this a thousand times,
+              <span class="italic"
+                >"its not about what you know, rather who you know"</span
+              >
+            </p>
+            <p>
+              In the job world where you are competing against several hundred
+              or even thousand candidates, having a solid network can be factor
+              of you getting the decision or not.
+            </p>
+            <h3 class="subheader text-4xl font-extrabold">My Experience</h3>
+            <p>
+              Last summer I was blessed to have the opportunity to intern with
+              Apple in their Austin Campus. I had a lot of people ask me, "How'd
+              it?"
+            </p>
+            <p>
+              Being a self-taught developer with less than one year of
+              experience landing an internship with Apple? But how?
+            </p>
+            <h4 class="text-2xl font-extrabold">I Networked!</h4>
+            <p>
+              I reached out to some people when I got the position at Apple, and
+              key was that I did not reach out to them telling them to help me
+              get a job. I reached out to the with the interest of learning how
+              they got to their position. I also seeked guidance on how to learn
+              coding languages like JavaScript, and what approaches they took to
+              mastering their craft. Showing an interest in someones profession
+              makes them excited and want to talk to you. So go out there and
+              ask some people for some advice, then see where it takes you!
+            </p>
+            <p>
+              If you really want to stand out, when you get advice from someone
+              show them how you took their advice and implemented it. For
+              example, if someone told you "go watch this course", you can go
+              watch the course and come back to them with a certificate of
+              completion and a casual review of what you thought of the course.
+            </p>
+          </div>
+        </div>
+        <div
+          class="content-header h-[200px] items-center flex justify-evenly gap-5"
+        >
+          <p class="text-[120px] text-black font-extrabold">3.</p>
+          <p class="section-header text-5xl font-extrabold text-black">
+            Update your LinkedIn... Please.
+          </p>
+        </div>
+        <div class="explanation-container flex justify-center">
+          <div class="w-[700px] flex flex-col gap-5">
+            <p>
+              Recruiters, hiring managers, engineers, and everyone on the job
+              market has a LinkedIn.
+            </p>
+            <p>
+              It is a great way to show off your experiences all while getting
+              to know and reaching out to people.
+            </p>
+            <p>You can look up people by company, name, school etc,.</p>
+            <h4 class="text-2xl font-extrabold">So how should I use it?</h4>
+            <p>
+              I recommend searching people at companies you want go to, and
+              using the school feature to look up alumni from your school. This
+              way, it instantly gives you something to connect over and can be a
+              great avenue for conversation.
+            </p>
+            <p>Take Advantage of the alumni search feature!</p>
+            <p>
+              Look up hiring manager for particular jobs, or connect with
+              recruiters at companies you are seeking.
+            </p>
+            <h4 class="text-2xl font-extrabold">
+              Keep your Profile up to Date
+            </h4>
+            <p>
+              Some things to do for your LinkedIn are add a profile picture, add
+              the relevant skills, jobs, descriptions, bio, portfolio pieces,
+              resume, educational background, extracirriculars etc,. View
+              LinkedIn as a social media for your resume, allowing you to share
+              your experiences on a professional platform with other recruiters,
+              job seekers, and more.
+            </p>
+            <p>
+              Also, for your profile picture you can add a open to work banner
+              for recruiters.
+            </p>
+          </div>
+        </div>
+        <div
+          class="content-header h-[200px] items-center flex justify-evenly gap-5"
+        >
+          <p class="text-[120px] text-black font-extrabold">4.</p>
+          <p class="section-header text-5xl font-extrabold text-black">
+            Cover Letters.
+          </p>
+        </div>
+        <div class="explanation-container flex justify-center">
+          <div class="w-[700px] flex flex-col gap-5">
+            <p>
+              Cover letters are a great way to make you stand out to companies
+              and recruiters.
+            </p>
+            <p>But let's be honest...</p>
+            <p>
+              <span>THEY'RE A PAIN!</span> Having to make a customer cover
+              letter tailored to your job application is time consuming. But
+              let's keep this in mind, it's your
+              <span class="font-extrabold">dream job</span> for a reason, right?
+            </p>
+            <p>
+              Recruiters can spot a generic cover letter in no time, so let's
+              see how we can make a great cover letter.
+            </p>
+            <h4 class="text-2xl font-extrabold">Open Strong</h4>
+            <p>
+              Lead with a strong opening setence about who you are and why you
+              are deserving of this particular role. After doing this, talk
+              about your experiences but keep in mind to not rehash your resume.
+              Keep it to one or two sentences of background.
+            </p>
+            <h4 class="text-2xl font-extrabold">Time to Brag!</h4>
+            <p>
+              Emphasize your personal value, and what makes you a great fit for
+              this role. Hiring managers want people to help them solve a
+              problem, so what makes you qualified to help them solve it? What
+              projects have you worked on that are relevant to the job posting?
+              What skills have your experiences lead you to learn?
+            </p>
+            <h4 class="text-2xl font-extrabold">Keep it short</h4>
+            <p>
+              Cover letters are a buffed up elevator speech. Ultimately it is
+              something for a recruiter to glance over and get to know a little
+              more about you. Get some feedback on the cover letter from some
+              friends, have a positive tone in your writing and reiterate over
+              it numerous times.
+            </p>
+          </div>
+        </div>
+        <div
+          class="content-header h-[200px] items-center flex justify-evenly gap-5"
+        >
+          <p class="text-[120px] text-black font-extrabold">5.</p>
+          <p class="section-header text-5xl font-extrabold text-black">
+            Stay Consistent.
+          </p>
+        </div>
+        <div class="explanation-container flex justify-center">
+          <div class="w-[700px] flex flex-col gap-5">
+            <p>Personal tip from me, but stay consistent.</p>
+            <p>
+              Have a goal in mind and work towards it day in and day out. Figure
+              out ways to make you stand out. Be self-aware, and realize the
+              things you need to learn to get to where you want to me. Seek
+              guidance and mentorship from those who have achieved what you
+              desire.
+            </p>
+            <p>
+              Learn to embrace not being the smartest one in the room. Accept
+              that there always will be people out there that have more
+              experience, go to a better school, have a better GPA, and so on.
+              However if you set your sight on a place you want to be, then you
+              will get there as long as you put the work in.
+            </p>
+            <p>
+              These tips above are to get you the opportunity, to allow you to
+              get a foot in the door. We can often feel lost and not know what
+              to do, however what worked for me is just doing something. Taking
+              some sort of initiative to try something new. Venture into the
+              unkown, and get comfortable with the feeling of being
+              uncomfortable. Once you start embracing the challenge of learning
+              something new, great things will come. After all, good things
+              don't come easy!
+            </p>
+            <h4 class="subheader font-extrabold text-3xl mt-10">
+              Thanks for Reading, hope this helps!
+            </h4>
+            <p>Feel free to connect with me on LinkedIn as well!</p>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-export default defineComponent({
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-});
-</script>
+<style scoped>
+.section-header,
+.subheader {
+  font-family: "Roboto Slab", serif;
+}
+</style>
